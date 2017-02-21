2523e4280cd8bfde609a9d0cc675d0b5c8e1f33a
Tane Yoroshi <taneyoroshi@gmail.com>
2017-02-21 23:48:52 +0800

    Implemented voting endpoint.

--- VotingService/Startup.cs
+++ VotingService/Startup.cs
@@ -12,6 +12,9 @@ namespace VotingService
             // Configure Web API for self-host. 
             HttpConfiguration config = new HttpConfiguration();
 
+            config.MapHttpAttributeRoutes();
+            config.Formatters.Add(new HtmlMediaFormatter());
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
